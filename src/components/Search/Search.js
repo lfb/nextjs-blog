04826788e7ba264bbad96c23b2a35af78ef99296
BobbyLiang <itbo@163.com>
2024-08-22 23:17:08 +0800

    style: 搜索样式优化

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,10 @@
 'use client'
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { getSearchURL } from '@/lib/regular-url'
 
 function Search({ isM }) {
+    const router = useRouter()
     const [keyword, setKeyword] = useState('')
 
     const handleSearchInputChange = e => {
@@ -10,10 +13,8 @@ function Search({ isM }) {
 
     const handleSearchKeyDown = e => {
         if (keyword && e.key === 'Enter') {
+            router.push(getSearchURL(keyword))
             setKeyword('')
-            setTimeout(() => {
-                window.location.href = `/search?keyword=${encodeURIComponent(keyword)}`
-            })
         }
     }
 
@@ -27,6 +28,7 @@ function Search({ isM }) {
                     search-input placeholder:text-sm placeholder:text-slate-400
                     focus:outline-none
                     focus:border-primary
+                    focus:bg-white
                     focus:shadow
                      text-sm
                       border
--- src/lib/regular-url.js
+++ src/lib/regular-url.js
@@ -1,11 +1,22 @@
+// 文章URL
 export function getArticleURL(article) {
     const { id, article_path } = article || {}
+
+    if (!id) return '/articles'
 
     return `/articles/${id}/${article_path}`
 }
 
+// 分类链接
 export function getCategoryURL(category) {
     const { id, category_key } = category || {}
 
+    if (!id) return '/'
+
     return `/category/${id}/${category_key}`
+}
+
+// 搜索URL
+export function getSearchURL(keyword = '') {
+    return `/search?keyword=${encodeURIComponent(keyword)}`
 }
